280606e4fabdc8214a2606213a3b05612376ac0b
Rutvij Jethwa <rutvijjethwa@gmail.com>
2017-04-11 15:49:29 +0530

    Algorithm chooser

--- hashutil.py
+++ hashutil.py
@@ -6,6 +6,9 @@ from optparse import OptionParser
 #############################################################
 # File Hasher
 def fileHasher(fileToHash):
+    """Return a string of Hash
+    Takes file object as input. 
+    Make sure to open the file in read-binary mode. for ex. open('abc.txt','rb')"""
     sha = hashlib.sha256()
     try:
         with open(file=fileToHash, mode="rb") as fileObject:
@@ -18,6 +21,9 @@ def fileHasher(fileToHash):
 ##############################################################
 # Option Parser
 def decorator(arg1):
+    """Decorator to warp Hashing module
+    Return Hashed string
+    """
     def choice(func):
         print("Argument for Hashing {}".format(arg1))
         x = func()
@@ -38,34 +44,34 @@ def decorator(arg1):
 
 ###########################
 def hashMD5():
+    """Returns and MD5 Hashlib Object"""
     hashObject = hashlib.md5()
     return hashObject
 
-
 def hashSHA1():
+    """Returns and SHA1 Hashlib Object"""
     hashObject = hashlib.sha1()
     return hashObject
 
-
 def hashSHA224():
+    """Returns and SHA224 Hashlib Object"""
     hashObject = hashlib.sha224()
     return hashObject
 
-
 def hashSHA256():
+    """Returns and SHA256 Hashlib Object"""
     hashObject = hashlib.sha256()
     return hashObject
 
-
 def hashSHA384():
+    """Returns and 384 Hashlib Object"""
     hashObject = hashlib.sha384()
     return hashObject
 
-
 def hashSHA512():
+    """Returns and SHA512 Hashlib Object"""
     hashObject = hashlib.sha512()
     return hashObject
-
 
 # decorator(stringHash)(hashMD5) #solution
 #################################################################
@@ -90,7 +96,7 @@ if __name__ == '__main__':
     if options.filename is not None:
         print("Hash for the file '{0}' is {1}".format(options.filename, fileHasher(options.filename)))
         # print("Hash for the file '{0}' is {1}".format(options.filename, decorator(options.filename)(hashSHA256)))
-        #
+
         if options.inputHash is not None:
             print('Compare string MATCHED') if (options.inputHash == fileHasher(options.filename)) else print(
                 "Compare string does NOT MATCH")
